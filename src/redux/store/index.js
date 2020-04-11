1ec746d1a64cd19184a8f53ba00f11ea11314ce0
nathanial292 <nathanial55@gmail.com>
2020-04-11 17:39:52 +0100

    Added support for redux web plugin

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 
@@ -6,12 +6,10 @@ import rootReducer from '../reducers'
 
 const loggerMiddleware = createLogger()
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+)));
 
 export default store
